37860414dc2136e43d9812877ef34fe17799d5f2
Debraj <dev.2293@gmail.com>
2020-05-13 12:19:21 -0700

    Fixings

--- Assignment 3/Hello.c
+++ Assignment 3/Hello.c
@@ -4,12 +4,16 @@
 int main(int argc, char *argv[])
 {
 
-	int rank, size, provided;
+	int rank, size, i, provided;
+	float A[10];
 
 	MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
 
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	printf("My rank %d of %d\n", rank, size);
+  //for(i=0; i<10; i++)
+  //  A[i] = i*rank;
+
+  printf("My rank %d of %d\n", rank, size);
 }
