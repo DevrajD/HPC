f204068a27f9ab0eb37b3dab30fa1d217379afe0
DevrajD <dev.2293@gmail.com>
2020-05-26 18:56:37 +0200

    x

--- HPC_DD2356_Final_Project/Source/runAll.sh
+++ HPC_DD2356_Final_Project/Source/runAll.sh
@@ -8,7 +8,7 @@
 # The name of the script is myjob
 #SBATCH -J myjobAll
 
-#SBATCH -t 00:35:00
+#SBATCH -t 00:5:00
 
 # Number of nodes
 #SBATCH --nodes=4
@@ -21,28 +21,10 @@
 #SBATCH -C Haswell
 
 
-cc -O2 Hello.c -o hello -lm
-cc -O2 pi_parallel_blocking.c -o piblockin -lm
-cc -O2 pi_parallel_nonblocking.c -o pinonblockin -lm
-cc -O2 pi_parallel_binaryReduc.c -o pibinaryreduc -lm
-cc -O2 pi_reduce.c -o piReduce -lm
-cc -O2 pi_gather.c -o piGather -lm
-cc -O2 pi_windows.c -o piWindows -lm
+cc -O2 mainFox.c -o Fox -lm -D N=3
 
-for i in 8 16 32 64 128
+for i in 9
 do
 rm my_output_fil$i
 srun -n $i ./hello >> my_output_fil$i
-rm my_output_piBlockParallel$i
-srun -n $i ./piblockin >> my_output_piBlockParallel$i
-rm my_output_pi_parallel_nonblocking$i
-srun -n $i ./pinonblockin >> my_output_pi_parallel_nonblocking$i
-rm my_output_piBinaryTReduc$i
-srun -n $i ./pibinaryreduc >> my_output_piBinaryTReduc$i
-rm my_output_piReduce$i
-srun -n $i ./piReduce >> my_output_piReduce$i
-rm my_output_piGather$i
-srun -n $i ./piGather >> my_output_piGather$i
-rm my_output_piWindows$i
-srun -n $i ./piWindows >> my_output_piWindows$i
 done
