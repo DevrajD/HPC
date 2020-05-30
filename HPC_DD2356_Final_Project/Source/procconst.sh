02c527702e35a154558b79df700dc5e9b7b7bf70
DevrajD <dev.2293@gmail.com>
2020-05-30 10:43:19 +0200

    .

--- HPC_DD2356_Final_Project/Source/procconst.sh
+++ HPC_DD2356_Final_Project/Source/procconst.sh
@@ -43,3 +43,23 @@ do
 
     done
 done
+
+DEBUG=1
+module swap PrgEnv-cray PrgEnv-intel 
+module swap intel intel/19.0.1.144
+pkg-config --cflags --libs mkl-dynamic-ilp64-iomp
+
+for j in 2 3 4 5 6
+do
+    PROCESSES=$(( $j * $j ))
+    for i in 2 4 6 8 10 16 20 25 32 64 128
+    do
+        N_BAR=$(( $i ))
+        N=$(( $(bc <<< "scale=0; sqrt($PROCESSES)") * $N_BAR ))
+
+        rm my_MKLoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+        cc -O3 mklFox.c -o mlkFOX -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=$DEBUG `pkg-config --libs --cflags mkl-dynamic-ilp64-iomp`
+        srun -n $PROCESSES ./mlkFOX >> my_MKLoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+
+    done
+done
