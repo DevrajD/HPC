ef1d5c20a78719c13dc2281a53f82727a7a9a713
DevrajD <dev.2293@gmail.com>
2020-05-30 20:25:52 +0200

    x

--- HPC_DD2356_Final_Project/Source/interactive runs.sh
+++ HPC_DD2356_Final_Project/Source/interactive runs.sh
@@ -57,3 +57,39 @@ do
 
     done
 done
+
+
+salloc --nodes=1 -t 01:00:00 -A edu20.DD2356 
+module swap PrgEnv-cray PrgEnv-intel 
+module swap intel intel/19.0.1.144
+DEBUG=0
+#CONST N RUNS
+for N in 2 4 6 8 9 10 12 14 16 25 32 49 50 64 90 128 256 512 768 1024 1500 2048 2100
+do
+    j=2
+    while [ $j -le 8 ]
+    do
+        if [ $(( $N % $j )) == 0 ]
+        then
+            PROCESSES=$(( $j * $j ))
+            for i in 2 4 6 8 10 16 20 25 32 64 128 256 512 1024 50 80 100 300 700 1500 #2048 is taking much longer
+            do
+                N_BAR=$(( $N / $j ))
+                #N=$(( $(bc <<< "scale=0; sqrt($PROCESSES)") * $N_BAR ))
+
+                rm my_constProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+                cc -O2 mainFox.c -o Foxp -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=$DEBUG
+                srun -n $PROCESSES ./Foxp >> my_constProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+
+                rm my_constProcoutputCOMM_files${N}_${N_BAR}_PRO${PROCESSES}
+                cc -O2 mainFox.c -o FoxpCOMM -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=0 -D COMMS=1 #This is only to check for communication overheads, hence ignoring DEBUG mode
+                srun -n $PROCESSES ./FoxpCOMM >> my_constProcoutputCOMM_files${N}_${N_BAR}_PRO${PROCESSES}
+
+                rm my_OPconstProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+                cc -O3 optimFox.c -o OPFoxp -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=$DEBUG -fopenmp
+                srun -n $PROCESSES ./OPFoxp >> my_OPconstProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+            done
+        fi
+        $j=$(( $j + 1 ))
+    done
+done
