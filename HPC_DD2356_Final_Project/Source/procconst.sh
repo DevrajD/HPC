5d11c87c15f322fde0b9852d667a039da0caaf3f
DevrajD <dev.2293@gmail.com>
2020-05-31 08:40:24 +0200

    .

--- HPC_DD2356_Final_Project/Source/procconst.sh
+++ HPC_DD2356_Final_Project/Source/procconst.sh
@@ -24,27 +24,34 @@
 PROCESSES=9 #Processes variable must be a perfect square value
 DEBUG=0
 
-for j in 2 3 4 5 6 8 9 10 11 12 14 16
+# for j in 2 3 4 5 6 8 9 10 11 12 14 16
+# do
+#     PROCESSES=$(( $j * $j ))
+#     for i in 2 4 6 8 10 16 20 25 32 64 128 256 512 1024 50 80 100 300 700 1500 #2048 is taking much longer
+#     do
+
+for N in 4 6 8 9 10 12 14 16 25 32 49 50 64 90 128 256 512 768 1024 1500 2048 2100 16800
 do
-    PROCESSES=$(( $j * $j ))
-    for i in 2 4 6 8 10 16 20 25 32 64 128 256 512 1024 50 80 100 300 700 1500 #2048 is taking much longer
+    j=2
+    while [ $j -le 8 ]
     do
-        N_BAR=$(( $i ))
-        N=$(( $(bc <<< "scale=0; sqrt($PROCESSES)") * $N_BAR ))
+        if [ $(( $N % $j )) == 0 ]
+        then
+            PROCESSES=$(( $j * $j ))
+            N_BAR=$(( $N / $j ))
 
-        rm my_constProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
-        cc -O2 mainFox.c -o Foxp -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=$DEBUG
-        srun -n $PROCESSES ./Foxp >> my_constProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+            rm my_constProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+            cc -O2 mainFox.c -o Foxp -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=$DEBUG
+            srun -n $PROCESSES ./Foxp >> my_constProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
 
-        rm my_constProcoutputCOMM_files${N}_${N_BAR}_PRO${PROCESSES}
-        cc -O2 mainFox.c -o FoxpCOMM -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=0 -D COMMS=1 #This is only to check for communication overheads, hence ignoring DEBUG mode
-        srun -n $PROCESSES ./FoxpCOMM >> my_constProcoutputCOMM_files${N}_${N_BAR}_PRO${PROCESSES}
+            rm my_constProcoutputCOMM_files${N}_${N_BAR}_PRO${PROCESSES}
+            cc -O2 mainFox.c -o FoxpCOMM -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=0 -D COMMS=1 #This is only to check for communication overheads, hence ignoring DEBUG mode
+            srun -n $PROCESSES ./FoxpCOMM >> my_constProcoutputCOMM_files${N}_${N_BAR}_PRO${PROCESSES}
 
-        rm my_OPconstProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
-        cc -O3 optimFox.c -o OPFoxp -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=$DEBUG -fopenmp
-        srun -n $PROCESSES ./OPFoxp >> my_OPconstProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
-
-
+            rm my_OPconstProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+            cc -O3 optimFox.c -o OPFoxp -lm -D N=$N -D N_BAR=$N_BAR -D DEBUG=$DEBUG -fopenmp
+            srun -n $PROCESSES ./OPFoxp >> my_OPconstProcoutput_files${N}_${N_BAR}_PRO${PROCESSES}
+        fi
     done
 done
 
